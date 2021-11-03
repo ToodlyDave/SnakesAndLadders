5971459e7ea749caec6950ef027587e54a876e17
ToodlyDave <alapatdavid@gmail.com>
2021-11-03 12:33:57 +0530

    Position set to 0

---
+++ src/com/game/SnakesAndLadders.java
@@ -0,0 +1,14 @@
+package com.game;
+
+public class SnakesAndLadders {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int position = 0;
+		System.out.println(" Welcome player!! ");
+		
+		System.out.println(" Current position: " + position);
+	}
+
+}
